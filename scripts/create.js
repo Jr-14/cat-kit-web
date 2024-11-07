44a06da8f0dc1cf54a98f8412900f5a537b64796
John Emerson Requizo <john.requizo@doshii.io>
2024-11-07 17:31:54 +1100

    Modify script file for creating cats db

--- scripts/create.js
+++ scripts/create.js
@@ -3,26 +3,25 @@ import fs from "node:fs";
 
 const BASE_PATH = "./db";
 
-const run = async () => {
-    const dbFileName = "testDb.sqlite";
-    if (!fs.existsSync(`${BASE_PATH}/${dbFileName}`)) {
-
-    }
-    const db = new sqlite3.Database("testDb.sqlite");
+const createCatTable = (db) => {
+    const createTableQuery = `CREATE TABLE IF NOT EXISTS Cats (
+    id INTEGER NOT NULL PRIMARY KEY
+    , name TEXT
+    , description TEXT
+    , dateOfBirth TEXT
+    , sex TEXT
+    , breed TEXT
+);`;
 
     db.serialize(() => {
-        db.run("CREATE TABLE lorem (info TEXT)")
-
-        const stmt = db.prepare("INSERT INTO lorem VALUES(?)");
-        for (let i = 0; i < 10; i++) {
-            stmt.run("Ipsum " + i);
-        }
-        stmt.finalize();
-
-        db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-            console.log(row.id + ": " + row.info)
-        });
+        db.run(createTableQuery);
     });
+};
+
+const run = async () => {
+    const db = new sqlite3.Database("testDb.sqlite");
+
+    createCatTable(db);
 
     db.close();
 };
