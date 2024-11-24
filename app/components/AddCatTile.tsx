15356066657cb2826c4e171c2a3c9c80f4255c7d
John Emerson Requizo <jr.jemerson14@gmail.com>
2024-11-24 22:41:36 +1100

    Run pretty

--- app/components/AddCatTile.tsx
+++ app/components/AddCatTile.tsx
@@ -20,17 +20,29 @@ import { CatDetails } from "~/model/CatsModel";
 
 type Props = PropsWithChildren<{}>;
 
-const AddCatTile: React.FC = ({
-
-} : Props
-) => {
+const AddCatTile: React.FC = ({}: Props) => {
   return (
-      <button className="tile-add-cat enlarge-on-hover" type="submit">
-      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12H18M12 6V18" stroke="#555555" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-        <p>
-          Add a cat
-        </p>
-      </button>
+    <button className="tile-add-cat enlarge-on-hover" type="submit">
+      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          {" "}
+          <path
+            d="M6 12H18M12 6V18"
+            stroke="#555555"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>{" "}
+        </g>
+      </svg>
+      <p>Add a cat</p>
+    </button>
   );
 };
 
