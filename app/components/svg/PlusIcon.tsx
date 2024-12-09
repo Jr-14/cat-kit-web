0d46ae84cacf04d2613c97e670ccc226fdeda2f3
John Emerson Requizo <jr.jemerson14@gmail.com>
2024-12-09 22:55:12 +1100

    Remove unused AddCatTile component. Refactored plus icon into react component. Typecheck

---
+++ app/components/svg/PlusIcon.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const PlusIcon: React.FC = () => {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M6 12H18M12 6V18"
+          stroke="#555555"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+};
+
+export default PlusIcon;
--- app/routes/cats._index.tsx
+++ app/routes/cats._index.tsx
@@ -4,6 +4,7 @@ import type { CatDetails, CatDetail } from "~/schemas/catSchema";
 import TileCats from "~/components/CatTiles";
 import { useMemo, useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
+import PlusIcon from "~/components/svg/PlusIcon";
 
 export const loader = async () => {
   const data = await getCatsDetails();
@@ -113,28 +114,7 @@ export default function CatLayout() {
         <div className="tiles-layout">
           <div className="tile tile-add-cat enlarge-on-hover">
             <Link to={`/cats/new`}>
-              <svg
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></g>
-                <g id="SVGRepo_iconCarrier">
-                  {" "}
-                  <path
-                    d="M6 12H18M12 6V18"
-                    stroke="#555555"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  ></path>{" "}
-                </g>
-              </svg>
+              <PlusIcon />
               <p>Add a cat</p>
             </Link>
           </div>
